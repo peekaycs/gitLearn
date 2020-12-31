f6da9db114137c98aa78e6885c79470f374adc78
pramod singh <pramod.singh@power2sme.com>
2020-12-31 23:03:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer( (req, res) => {
+
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+	let extname = path.extname(filePath);
+	let contentType = 'text/html';
+	switch(extname){
+		case '.js':
+			contentType = 'text/javascript';
+		break;
+		case '.css':
+			contentType = 'text/css';
+		break;		
+		case '.png':
+			contentType = 'image/png';
+		break;
+		case '.jpg':
+			contentType = 'image/jpg';
+		break;case '.json':
+			contentType = 'application/json';
+	}
+	// read requested file
+	fs.readFile(filePath, (err, content) => {
+		if(err){
+			if(err.code === 'ENOENT'){
+				// page not found
+				fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+					res.writeHead(200, {'Content-Type':'text/html'})
+					res.end(content,'utf8')
+				})
+			}else{
+				res.writeHead(500)
+				res.end(`Server Error: ${err.code}`)
+			}
+		}else{
+			// in case of success
+			res.writeHead(200, {'Content-Type' : contentType})
+			res.end(content,'utf8')
+		}
+	})
+})
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
