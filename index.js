7b8bede39ff579812e846965443b84d9fc7870d5
peekaycs <56191053+peekaycs@users.noreply.github.com>
2021-01-09 09:11:05 +0530

    Update index.js
    port number changed

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ const server = http.createServer( (req, res) => {
 	})
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
