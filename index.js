c589aa2c639d5c67f007f18c182b56a00b339e1d
peekaycs <56191053+peekaycs@users.noreply.github.com>
2021-01-09 10:22:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ const server = http.createServer( (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-// this is not useful
+// this is not required att all
