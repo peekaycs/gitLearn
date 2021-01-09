a5e3ad8b364e5cde89ad74e219cfa45ba2ec6902
pramod singh <pramod.singh@power2sme.com>
2021-01-09 10:26:16 +0530

    index.js changed

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ const server = http.createServer( (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-// this is not useful
+// I know this is not required
