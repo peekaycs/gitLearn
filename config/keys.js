484124531879d2464f2a25aa894fc50162a47f25
peekaycs <56191053+peekaycs@users.noreply.github.com>
2021-01-29 10:27:03 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-	mongoURI:'mongodb+srv://pramod:pramod123@cluster0.fgow5.mongodb.net/node-angular?retryWrites=true&w=majority'
+	mongoURI:'mongodb+srv://<user>:<password>@cluster0.fgow5.mongodb.net/node-angular?retryWrites=true&w=majority'
 }
